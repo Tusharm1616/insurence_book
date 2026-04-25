8894b27ea44c3c616aec09c9e610f9dbac0e7f52
Tusharm1616 <tusharm1616@example.com>
2026-04-26 00:56:55 +0530

    fix: change relative import to absolute import in models/policies.py

--- models/policies.py
+++ models/policies.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from ..database import Base
+from database import Base
 
 class Policy(Base):
     __tablename__ = "policies"
